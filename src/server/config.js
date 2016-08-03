f430956609f0eb569892578b4e037c99d88d1525
Alex Stark <alexcstark@gmail.com>
2016-08-03 16:59:22 -0700

    dev start

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,6 @@
 export const PORT = process.env.PORT || 8000;
+import express from 'express';
+import path from 'path';
 
 export const configureServer = function(app) {
   app.use(express.static(path.join(__dirname, '/../client')));
