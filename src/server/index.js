265357555a0cc3d49d47623f813f1de79d8a7647
Alex Stark <alexcstark@gmail.com>
2016-08-03 12:25:49 -0700

    basic express server

---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.send('You\'re here! Thanks for coming!');
+});
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
