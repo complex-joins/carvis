d706b5e468c43d4c4e2ccbc7a803dbfadd42b2c6
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-13 12:17:10 -0700

    remove requestride endpoint, moving to api server

--- carvis-web/src/server/routes/authRoutes.js
+++ carvis-web/src/server/routes/authRoutes.js
@@ -33,29 +33,4 @@ export default function (app) { // LYFT 2FA - first call sends SMS to user
     // Basic auth
   });
 
-  app.post('/request/ride/:vendor', (req, res) => {
-    let vendor = req.params.vendor;
-
-    // if the vendor is uber/lyft call the relevant book ride.
-
-    if (vendor === 'uber') {
-      // function (userLocation, token, destination)
-      uberHelper.confirmPickup({ lat: '37.7773563', lng: '-122.3968629' }, '', { lat: '37.7773563', lng: '-122.3968629' }); // TODO: dynamic
-      console.log('requesting uber ride');
-      res.json({ message: 'on its way' });
-
-    } else if (vendor === 'lyft') {
-      // function (token, origin, destination, paymentInfo, partySize, rideId)
-      lyftHelper.getCost('', { lat: '', lng: '', routableAddress: '' }, { lat: '', lng: '', routableAddress: '' }, '', 1, ''); // TODO: dynamic.
-      console.log('requesting lyft ride');
-      res.json({ message: 'on its way' });
-
-      // if vendor isn't uber/lyft return a 404 not found.
-    } else {
-      console.log('invalid vendor');
-      res.status(404)
-        .send();
-    }
-  });
-
 }
