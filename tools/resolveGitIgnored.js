1365113430ac1ee83d4df7351ac88b20fd68bdd2
Alex Stark <alexcstark@gmail.com>
2016-08-15 19:00:49 -0700

    additions to gitignored

--- tools/resolveGitIgnored.js
+++ tools/resolveGitIgnored.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const shell = require('shelljs');
 
-const PRIVATE_DIR = path.join(__dirname, '/../../../carvis-private-info');
-
+const PRIVATE_DIR = path.join(__dirname, '/../../carvis-private-info');
+console.log(PRIVATE_DIR);
+makePrivateDirsIfNeeded();
 let fileDirectory = fs.readdirSync(PRIVATE_DIR);
+updatePrivateDirectory(fileDirectory);
 fileDirectory = fileDirectory.filter((file) => file[0] !== '.');
 fileDirectory.forEach((file) => {
   let currentFile = path.join(PRIVATE_DIR, `/${file}`);
@@ -15,3 +18,24 @@ fileDirectory.forEach((file) => {
   console.log(filePath);
   fs.createReadStream(currentFile).pipe(fs.createWriteStream(path.join(__dirname, `/../${filePath}`)));
 });
+
+function makePrivateDirsIfNeeded() {
+  let searchDir = path.join(__dirname, '/../../');
+  let thisDir = path.join(__dirname, '/../');
+  if (fs.readdirSync(thisDir).indexOf('secret') === -1) {
+    shell.exec('mkdir secret');
+  }
+  if (fs.readdirSync(searchDir).indexOf('carvis-private-info') === -1) {
+    shell.cd('..');
+    shell.exec('git clone https://github.com/alexcstark/carvis-private-info.git');
+    shell.exec('git remote add upstream https://github.com/alexcstark/carvis-private-info.git');
+    shell.cd('carvis-private-info');
+  }
+}
+function updatePrivateDirectory(directory) {
+  console.log(directory);
+  let pwd = shell.pwd();
+  shell.cd('../carvis-private-info');
+  shell.exec('git pull origin master');
+  shell.cd(pwd);
+}
