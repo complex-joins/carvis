59ff9aea936e4c12bd0b2ddb9b6d4cf45e363701
Alex Stark <alexcstark@gmail.com>
2016-08-17 15:45:32 -0700

    lean main web

--- src/server/utils/uber-helper.js
+++ src/server/utils/uber-helper.js
@@ -1,15 +1,17 @@
 var fetch = require('node-fetch');
-var uberMethods = require('./uberPrivateMethods');
-var baseURL = 'https://cn-sjc1.uber.com';
-var APItoken = require('./../../../secret/config.js')
-  .CARVIS_API_KEY;
-var APIserver = require('./../../../secret/config.js')
-  .CARVIS_API;
+var uberMethods = !process.env.TRAVIS ? require('./uberPrivateMethods') : null;
+var baseURL = 'https://cn-sjc1.uber.com'; // https ?
+var APItoken = !process.env.TRAVIS ? require('../../../secret/config.js')
+  .CARVIS_API_KEY : null;
+var APIserver = !process.env.TRAVIS ? require('../../../secret/config.js')
+  .CARVIS_API : null;
 
-var login = function (username, password) {
+var login = function (username, password, userId) {
   var path = baseURL + uberMethods.login.path;
   var body = uberMethods.login.body(username, password);
   var headers = uberMethods.login.headers();
+
+  userId = userId || null; // alexaUserId
 
   fetch(path, {
       method: 'POST',
@@ -20,8 +22,32 @@ var login = function (username, password) {
       return res.json();
     })
     .then(function (data) {
-      var response = uberMethods.login.responseMethod(data);
-      // response.email for DB && response.token for subsequent calls.
+      var response = uberMethods.login.responseMethod(data, password, userId);
+
+      // POST THE USER DATA TO OUR RELATIONAL DATABASE
+      // var dbpostURL = 'http://' + APIserver + '/users/updateOrCreate';
+      var dbpostURL = 'http://localhost:8080/users/updateOrCreate';
+
+      console.log('response pre DB post', response);
+
+      fetch(dbpostURL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': APItoken
+          },
+          body: JSON.stringify(response)
+        })
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          console.log('success posting user', data);
+        })
+        .catch(function (err) {
+          console.warn('err posting user', err);
+        });
+
     })
     .catch(function (err) {
       console.log('ERROR login UBER', err);
@@ -56,7 +82,7 @@ var login = function (username, password) {
 //     });
 // };
 
-var confirmPickup = function (userLocation, token, destination) {
+var confirmPickup = function (userLocation, token, destination, rideId) {
   var path = baseURL + uberMethods.confirmPickup.path;
   var body = uberMethods.confirmPickup.body(destination, userLocation);
   var headers = uberMethods.confirmPickup.headers(userLocation, token);
@@ -72,7 +98,30 @@ var confirmPickup = function (userLocation, token, destination) {
     .then(function (data) {
       // DB post all data.
       var response = uberMethods.confirmPickup.responseMethod(data);
-      // do something client/non-gitignored side with response, cancel methods etc
+
+      // var dbpostURL = 'http://' + APIserver + '/rides/' + rideId;
+      var dbpostURL = 'http://localhost:8080/rides/' + rideId;
+
+      // once we receive the request-ride confirmation response
+      // we update the DB record for that ride with eta and vendorRideId
+      fetch(dbpostURL, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': APItoken
+          },
+          body: JSON.stringify(response)
+        })
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          console.log('success posting user', data);
+        })
+        .catch(function (err) {
+          console.warn('err posting user', err);
+        });
+
     })
     .catch(function (err) {
       console.log('ERROR login UBER', err);
