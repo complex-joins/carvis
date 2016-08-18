a66cf213a03298f5808c155922992bf26fdd423a
Alex Stark <alexcstark@gmail.com>
2016-08-17 20:24:25 -0700

    fix test

--- test/testServer.js
+++ test/testServer.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { configureServer } from '../src/server/config';
-import staticFileRoutes from '../src/server/routes/staticFileRoutes';
+import { configureServer } from '../dist/server/config';
+import staticFileRoutes from '../dist/server/routes/staticFileRoutes';
 // import authRoutes from '../src/server/routes/authRoutes';
 
 const app = express();
