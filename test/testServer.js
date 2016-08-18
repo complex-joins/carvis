915fb7404ae25b42d2c907ab11c05c40a4d43405
Alex Stark <alexcstark@gmail.com>
2016-08-17 20:19:36 -0700

    comment out auth routes - to be replaced with helper api

--- test/testServer.js
+++ test/testServer.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { configureServer } from '../src/server/config';
 import staticFileRoutes from '../src/server/routes/staticFileRoutes';
-import authRoutes from '../src/server/routes/authRoutes';
+// import authRoutes from '../src/server/routes/authRoutes';
 
 const app = express();
 
@@ -9,6 +9,6 @@ configureServer(app);
 
 // Set up routes
 staticFileRoutes(app);
-authRoutes(app);
+// authRoutes(app);
 
 export default app;
