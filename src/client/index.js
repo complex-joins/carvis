c912c13acff31b04c35d00fc5b796d4839ab43b9
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-08-24 15:15:10 -0700

    Protect /app route

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Splash from './splash-components/Splash';
 import Auth from './auth-components/Auth';
+import authHelper from './auth-components/auth-helpers';
 import {LyftAuth, UberAuth, Login} from './auth-components/AuthComponents';
 import MainLayout from './layout-components/MainLayout';
 import NoMatch from './layout-components/NoMatch';
@@ -13,11 +14,20 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 requireAll(require.context('./static/', true, /^\.\/.*/));
 function requireAll(r) { r.keys().forEach(r); }
 
+function requireAuth(nextState, replace) {
+  if (!authHelper.loggedIn()) {
+    replace({
+      pathname: '/auth',
+      state: { nextPathname: nextState.location.pathname }
+    });
+  }
+}
+
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={MainLayout}>
       <IndexRoute component={Splash}></IndexRoute>
-      <Route path="app" component={App}></Route>
+      <Route path="app" component={App} onEnter={requireAuth}></Route>
       <Route path="auth" component={Auth}>
         <Route path="login" component={Login}></Route>
       </Route>
