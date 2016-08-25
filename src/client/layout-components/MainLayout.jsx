7e6edd55037844e37b9fd65a8f9abbbef2b60ecb
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-08-24 18:14:43 -0700

    Resolve merge conflict

--- src/client/layout-components/MainLayout.jsx
+++ src/client/layout-components/MainLayout.jsx
@@ -1,13 +1,26 @@
+// TODO: add /login and /logout links to nav based on current state of loggedIn
+
 import React from 'react';
 import Nav from './Nav';
+// import authHelper from '../auth-components/auth-helpers';
 
 export default class MainLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inAuth: false
-    };
+    // this.state = {
+    //   loggedIn: authHelper.loggedIn()
+    // };
   }
+
+  // updateAuth(loggedIn) {
+  //   this.setState({
+  //     loggedIn: loggedIn
+  //   });
+  // }
+
+  // componentWillMount() {
+  //   authHelper.onChange = this.updateAuth;
+  // }
 
   render() {
     let currentLocation = this.props.location.pathname;
