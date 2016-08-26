4aa5acb84ea7f2c855aaba3a1be0b976138a5eca
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-25 21:34:28 -0700

    getEstimate on client map

--- src/client/order-components/GoogleAPI.js
+++ src/client/order-components/GoogleAPI.js
@@ -1,9 +1,30 @@
 // eslint-disable
 let map;
+const formatAnswer = (mode, value) => {
+  if (!value) {
+    return 'not available';
+  }
+  let winnerEstimate;
+  // convert estimate to $ or minutes
+  if (mode === 'fast') {
+    let minutes = Math.floor(value / 60);
+    winnerEstimate = minutes.toString() + ' minutes'; // always full minutes
+  } else {
+    let dollars = Math.floor(value / 100);
+    let cents = Math.floor(value % 100);
+    winnerEstimate = '$' + dollars.toString() + '.';
+    winnerEstimate += cents.toString();
+  }
+  return winnerEstimate;
+};
+
 export default function initMap() {
-  var origin_place_id = null;
-  var destination_place_id = null;
-  var travel_mode = 'DRIVING';
+  let origin_place_id = null;
+  let destination_place_id = null;
+  let travel_mode = 'DRIVING';
+  let origin = {};
+  let destination = {};
+  let userId = 1; // hardcoded.
 
   map = new google.maps.Map(document.getElementById('map'), {
     mapTypeControl: false,
@@ -14,19 +35,19 @@ export default function initMap() {
     zoom: 15
   });
 
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
   directionsDisplay.setMap(map);
 
-  var origin_input = document.getElementById('origin-input');
-  var destination_input = document.getElementById('destination-input');
+  let origin_input = document.getElementById('origin-input');
+  let destination_input = document.getElementById('destination-input');
 
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
 
-  var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+  let origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
   origin_autocomplete.bindTo('bounds', map);
-  var destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
+  let destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
   destination_autocomplete.bindTo('bounds', map);
 
   // Sets a listener on a radio button to change the filter type on Places
@@ -41,8 +62,8 @@ export default function initMap() {
     }
   }
 
-  origin_autocomplete.addListener('place_changed', function() {
-    var place = origin_autocomplete.getPlace();
+  origin_autocomplete.addListener('place_changed', function () {
+    let place = origin_autocomplete.getPlace();
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
@@ -52,11 +73,19 @@ export default function initMap() {
     // If the place has a geometry, store its place ID and route if we have
     // the other place ID
     origin_place_id = place.place_id;
-    route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
+
+    // set origin for getEstimate
+    let origin_lat = place.geometry.location.lat();
+    let origin_lng = place.geometry.location.lng();
+    origin.coords = [origin_lat, origin_lng];
+    origin.descrip = place.formatted_address;
+    // invoke.
+
+    route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay, origin, destination, userId);
   });
 
-  destination_autocomplete.addListener('place_changed', function() {
-    var place = destination_autocomplete.getPlace();
+  destination_autocomplete.addListener('place_changed', function () {
+    let place = destination_autocomplete.getPlace();
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
@@ -66,13 +95,79 @@ export default function initMap() {
     // If the place has a geometry, store its place ID and route if we have
     // the other place ID
     destination_place_id = place.place_id;
-    route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
+
+    // set destination for getEstimate
+    let destination_lat = place.geometry.location.lat();
+    let destination_lng = place.geometry.location.lng();
+    destination.coords = [destination_lat, destination_lng];
+    destination.descrip = place.formatted_address;
+    // invoke
+
+    route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay, origin, destination, userId);
   });
 
   function route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay) {
     if (!origin_place_id || !destination_place_id) {
       return;
     }
+    console.log('route invoked');
+
+    // local post to router, which will pass through to getEstimate.
+    // NOTE: local router will need to decrypt the production userId before CARVIS_API getEstimate post
+    let url = `http://localhost:8000/internal/getEstimate`; // hardcoded.
+    // let url = `http://${process.env.CARVIS_WEB_API}/internal/getEstimate`;
+    let body = (requestType) => {
+      let body = {
+        requestType: requestType,
+        origin: origin,
+        destination: destination,
+        carvisUserId: userId
+      };
+      return body;
+    };
+
+    // TODO: link up the ORDER car buttons
+
+    // Cost Estimate - web client -> web server -> main api -> Lyft/Uber
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'x-access-token': ''
+        },
+        body: JSON.stringify(body('cheap'))
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success getEstimate POST COST', data);
+        // display the relevant data
+        let carvisTime = document.getElementById('carvis-estimated-cost');
+        console.log(carvisTime);
+        carvisTime.innerHTML =
+          '<p class="pad-right no-margin"> Carvis Estimated Cost | Lyft: ' + formatAnswer('cheap', data.lyftEstimatedFare) + ' | Uber: ' + formatAnswer('cheap', data.uberEstimatedFare) + '<button class="black-text" id="order-cheapest-car">Order Cheapest Car</button></p> ';
+      })
+      .catch(err => console.warn('error in getEstimate POST COST', err));
+
+    // ETA Estimate - web client -> web server -> main api -> Lyft/Uber
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'x-access-token': ''
+        },
+        body: JSON.stringify(body('fast'))
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success getEstimate POST ETA', data);
+        // display the relevant data
+        let carvisTime = document.getElementById('carvis-estimated-time');
+        console.log(carvisTime);
+        carvisTime.innerHTML =
+          '<p class="pad-right no-margin"> Carvis Estimated Time | Lyft: ' + formatAnswer('fast', data.lyftEstimatedETA) + ' | Uber: ' + formatAnswer('fast', data.uberEstimatedETA) + '<button class="black-text" id="order-fastest-car">Order Fastest Car</button></p> ';
+      })
+      .catch(err => console.warn('error in getEstimate POST ETA', err));
+
     directionsService.route({
       origin: {
         'placeId': origin_place_id
@@ -81,17 +176,19 @@ export default function initMap() {
         'placeId': destination_place_id
       },
       travelMode: travel_mode
-    }, function(response, status) {
+    }, function (response, status) {
       if (status === 'OK') {
         console.log('driving response', response);
-        let time = document.getElementById('estimated-time');
+        let time = document.getElementById('google-estimated-time');
         console.log(time);
         time.innerHTML =
-        '<p class="pad-right no-margin"> Transit Time: '
-        + getEstimatedTime(response) +
-        '<button id="order-car">Order Car</button></p> ';
+          '<p class="pad-right no-margin"> Google Estimated Time: ' +
+          getEstimatedTime(response);
 
         directionsDisplay.setDirections(response);
+
+
+
       } else {
         window.alert('Directions request failed due to ' + status);
       }
---
+++ src/server/routes/rideRoutes.js
@@ -0,0 +1,97 @@
+const CARVIS_API_KEY = process.env.CARVIS_API_KEY;
+const CARVIS_API = process.env.CARVIS_API;
+const JWT_SECRET = process.env.JWT_SECRET;
+import fetch from 'node-fetch';
+import jwt from 'jsonwebtoken'; // used to create, sign, and verify tokens
+
+export default function (app) { // LYFT 2FA - first call sends SMS to user
+  app.post('/internal/getEstimate', (req, res) => {
+    // do the userId decryption
+
+    let url = `http://localhost:8080/web/estimate`;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': process.env.CARVIS_API_KEY
+        },
+        body: JSON.stringify(req.body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success POST getEstimate to API', data);
+        res.json(data);
+        // NOTE: in future shield sensitive info from client.
+        // similar to lyftPrivateMethods logic.
+      })
+      .then(err => console.warn('error POST getEstimate to API', err));
+  });
+
+  app.post('/internal/addRide', (req, res) => {
+    // do the userId decryption
+
+    let url = `http://localhost:8080/web/addRide`;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': process.env.CARVIS_API_KEY
+        },
+        body: JSON.stringify(req.body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success POST addRide to API', data);
+        // NOTE: in future shield sensitive info from client.
+        // similar to lyftPrivateMethods logic.
+        res.json(data);
+      })
+      .then(err => console.warn('error POST addRide to API', err));
+  });
+
+
+  app.post('/internal/requestRide', (req, res) => {
+    // do the userId decryption
+
+    let url = `http://localhost:8080/web/requestRide`;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': process.env.CARVIS_API_KEY
+        },
+        body: JSON.stringify(req.body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success POST requestRide to API', data);
+        // NOTE: in future shield sensitive info from client.
+        // similar to lyftPrivateMethods logic.
+        res.json(data);
+      })
+      .then(err => console.warn('error POST requestRide to API', err));
+  });
+
+  app.post('/internal/shareETA', (req, res) => {
+    // do the userId decryption
+
+    let url = `http://localhost:8080/web/shareETA`;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': process.env.CARVIS_API_KEY
+        },
+        body: JSON.stringify(req.body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success POST shareETA to API', data);
+        // NOTE: in future shield sensitive info from client.
+        // similar to lyftPrivateMethods logic.
+        res.json(data);
+      })
+      .then(err => console.warn('error POST shareETA to API', err));
+  });
+
+}
